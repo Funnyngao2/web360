86ceb3653c7b1115b9531e922fd9c190f8392f79
Funnyngao2 <23211TT0413@mail.tdc.edu.vn>
2025-07-02 16:44:30 +0000

    m

--- app.py
+++ app.py
@@ -281,5 +281,6 @@ def rename_project():
     except Exception as e:
         return jsonify({'error': f'Không thể đổi tên dự án: {str(e)}'}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway sẽ truyền PORT
+    app.run(host="0.0.0.0", port=port)
